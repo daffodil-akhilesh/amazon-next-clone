4376512f5d785d5994bd4b13fde6c2d2a82a8a1e
Akhilesh Pandey <akhilesh.pandey@unthinkable.co>
2021-04-13 17:28:50 +0530

    <Added Basic Project Structure>

---
+++ components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import constants from "../../constants";
+import classes from './Layout.module.css';
+
+const Layout = ({ children }) => {
+  return (
+    <div className={classes.Layout}>  
+      <Head>
+        <link rel="icon" href="amazon.ico" />
+        <title>{constants.HOME_TITLE}</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const constants = {
+    HOME_TITLE:"Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in"
+}
+
+export default constants;
