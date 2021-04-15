81360064acaa4f6b31daf875ac64d816e2483358
unthinkable-akhilesh <akhilesh.pandey@unthinkable.co>
2021-04-15 10:47:59 +0530

    <Added Redux setup>

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globalStyles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  let store = useStore(pageProps.initialReduxState);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
---
+++ redux/store.js
@@ -0,0 +1,53 @@
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { useMemo } from "react";
+import thunk from "redux-thunk";
+
+let store;
+
+const rootReducer = combineReducers({});
+const composeEnhancers =
+  process.env.ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+const logger = (store) => (next) => (action) => {
+  console.log(action.type);
+  return next(action);
+};
+
+const initStore = (preloadedState) => {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(logger, thunk))
+  );
+};
+
+export const initializeStore = (preloadedState) => {
+  // whichever is defined is returned
+  let _store = store ?? initStore(preloadedState);
+
+  // Navigating from one one page to other store should have updates
+  if (store && preloadedState) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+
+    // reset the current store
+    store = undefined;
+  }
+
+  // for ssg and ssr always get the new store
+  if (typeof window === "undefined") return _store;
+  // load the store once in the client
+  if (!store) store = _store;
+
+  //return updated store
+  return _store;
+};
+
+export const useStore = (initialState) => {
+  let store = useMemo(() => initializeStore(initialState), [initialState]);
+  return store;
+};
